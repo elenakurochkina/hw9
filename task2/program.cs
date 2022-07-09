55fe5d4f9837ab862cd6e5ec14e05e1bfff9e64f
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-07-09 21:03:27 +0300

    task2 completed

---
+++ task2/program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int SumNumbers(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+    return SumNumbers(M,N-1)+N;
+}
+System.Console.WriteLine(SumNumbers(1,3));
+
+
