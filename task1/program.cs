1f5abbcd7d7b25c198290e1050800cea3056aa8e
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-07-09 19:54:39 +0300

    task1 completed

---
+++ task1/program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все 
+//чётные натуральные числа в промежутке от M до N с помощью рекурсии
+
+void PrintNatural(int N, int M)
+{
+    if (M < N)
+    {
+        return;
+    }
+    if (N%2==0)
+{
+    System.Console.Write($"{N}; ");
+}
+    PrintNatural(N + 1, M);
+}
+
+PrintNatural(2, 20);
